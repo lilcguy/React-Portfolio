2529cb9c3d7e3098b04c05e2b657cc68cb87c34d
unknown <thecbradshaw89@gmail.com>
2023-03-24 15:40:57 -0500

    decided on handling navigation only in navigation component. moving on to creation of About.js page component and then will test.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,19 +6,26 @@
 import React, {useState} from 'react';
 
 export default function Navigation() {
-    const [currentPage, setCurrentPage] = useState('About Me'); //about me serves as the landing page
+    const [currentPage, setCurrentPage] = useState('About'); //about me serves as the landing page
 
-    const renderPage =(page) => {
-        if (currentPage === 'About Me') {
+    const renderPage =(page) => { //this function handles navigation with conditional page rendering.
+        if (page === 'About') {
             return <About />
         }
 
     }
 
+    function handlePageChange(page) {
+        setCurrentPage(page); //set the state of currentPage to param
+        renderPage(page); //run this function to check that and render the corresponding component.
+    }
+
     return (
         <div className="navbar">
             <nav>
-                <a>About Me</a> {/* these will take onClick attributes to handle the page changing with a function*/}
+                <a
+                href="#about"
+                onClick={() => handlePageChange('About')}>About Me</a> {/* these will take onClick attributes to handle the page changing with a function*/}
                 <a>Portfolio</a>
                 <a>Contact</a>
                 <a>Resume</a> 
@@ -29,3 +36,9 @@ export default function Navigation() {
 }
 
 
+// this page handles setting the state of currentPage.
+// somewhere that will be taken in as a prop to conditionally render about, portfolio, etc.
+// export currentPage as a prop...
+
+//add ternnary conditions in the JSX for current page highlighting in tab.
+    // in theory, just use the currentPage variable to check it.
