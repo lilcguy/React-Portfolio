7095099c08f02019f7b4feccb7eede24ecb60543
unknown <thecbradshaw89@gmail.com>
2023-03-24 15:25:06 -0500

    added pages folder, added files for about, conatct, portfolio, and resume. working on navigation component.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,24 @@
 //nav bar component. This will have conditionals based on what page the user is on to render 
-    //activity 24 portfolioContainer
+    //see activity 24 portfolioContainer
 // elements of the navbar, and highlight the navbar 
 // this component is exported to the Header component.
 
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Navigation() {
-    const [currentPage, setCurrentPage] = useState('Home');
+    const [currentPage, setCurrentPage] = useState('About Me'); //about me serves as the landing page
 
-    const renderPage =() => {
-        
+    const renderPage =(page) => {
+        if (currentPage === 'About Me') {
+            return <About />
+        }
+
     }
 
     return (
         <div className="navbar">
             <nav>
-                <a>About Me</a>
+                <a>About Me</a> {/* these will take onClick attributes to handle the page changing with a function*/}
                 <a>Portfolio</a>
                 <a>Contact</a>
                 <a>Resume</a> 
