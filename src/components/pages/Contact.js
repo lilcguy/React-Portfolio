4f1d07bd58e4f424e0ca31f78fd8f0efdfe535cd
unknown <thecbradshaw89@gmail.com>
2023-03-25 20:29:52 -0500

    added homebrew styling to contact content page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,15 +3,20 @@
 import React from 'react';
 
 import '../styles/Content-body.css';
+import '../styles/Contact.css';
 
 export default function Contact() {
     return (
         <div className="body"> 
-            <h1>Contact Me!</h1>
-            <textarea required>Your Name</textarea>
-            <textarea required>Your Email</textarea>
-            <textarea required>Message</textarea>
-
+            <h1 className="h1">Contact Me!</h1>
+            <div className="forms-parent">
+                <textarea className="form">Your Name</textarea>
+                <textarea className="form">Your Email</textarea>
+                <textarea className="msg-form">Message</textarea>
+            </div>
+            <h2 className="h1">999-999-9999</h2>
+            <h2 className="h1">thecbradshaw89@gmail.com</h2>
+            
         </div>
     )
 }
